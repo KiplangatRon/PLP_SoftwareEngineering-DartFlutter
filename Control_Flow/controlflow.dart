3d1fb5ade75f488ba48a9788a1b38edd3aaeb363
KiplangatRon <ronnykiplangat14@gmail.com>
2024-03-16 20:10:16 +0300

    Control Flows

---
+++ Control_Flow/controlflow.dart
@@ -0,0 +1,13 @@
+import 'dart:math';
+
+void main() {
+  var num = 1;
+  if (num > 10) {
+    print('Your number is greater than 10');
+  } else if (num < 10) {
+    print('Your number is less than 10');
+  } else {
+    print('Your number is equal to 10');
+  }
+}
+
